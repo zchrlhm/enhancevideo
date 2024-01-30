2116ac12338e1ee1f05478978efe93ea2ce98c6a
zchrlhm <158007124+zchrlhm@users.noreply.github.com>
2024-01-30 11:39:44 +0700

    Add files via upload

--- streamvideo.py
+++ streamvideo.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from PIL import Image, ImageEnhance
-from moviepy.editor 
-import VideoFileClip
+from moviepy.editor import VideoFileClip
 import numpy as np
 from io import BytesIO
 
